7c7738c4aef7ef55f8ff3075fb29655d4cde3c3c
Yury Philipov <yury.philipov@gmail.com>
2020-08-25 22:14:33 +0800

    app structure

--- webpack.config.js
+++ webpack.config.js
@@ -15,4 +15,12 @@ module.exports = {
       template: "./src/index.html",
     }),
   ],
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ["style-loader", "css-loader"],
+      },
+    ],
+  },
 };
